6771a4ed742820a5088897d97daaa60a8a210ffd
litleck <nwowens32@gmail.com>
2021-07-11 19:22:27 -0400

    Added WiderScrollbar plugin and updated SystemInfo plugin

--- SystemInfo/src/main/java/com/aliucord/plugins/SystemInfo.java
+++ SystemInfo/src/main/java/com/aliucord/plugins/SystemInfo.java
@@ -1,7 +1,10 @@
 package com.aliucord.plugins;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.os.Build;
+import android.os.SystemClock;
+import android.text.format.DateUtils;
 
 import androidx.annotation.NonNull;
 
@@ -19,21 +22,33 @@ public class SystemInfo extends Plugin {
         Manifest manifest = new Manifest();
         manifest.authors = new Manifest.Author[]{ new Manifest.Author("Möth", 289556910426816513L) };
         manifest.description = "Adds a systeminfo slash command that provides basic system information.";
-        manifest.version = "1.0.4";
+        manifest.version = "1.0.5";
         manifest.updateUrl = "https://raw.githubusercontent.com/litleck/aliucord-plugins/builds/updater.json";
         return manifest;
     }
 
     @Override
     public void start(Context context) {
-        commands.registerCommand("system-info", "Get system information", Collections.emptyList(), args -> {
+        commands.registerCommand("system-info", "Get system information", Collections.emptyList(), ctx -> {
             MessageEmbedBuilder embed = new MessageEmbedBuilder();
+
+            ActivityManager actManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+            actManager.getMemoryInfo(memInfo);
+            long totalMemory = memInfo.totalMem;
+
+            String uptime = DateUtils.formatElapsedTime(SystemClock.elapsedRealtime() / 1000);
 
             embed.setTitle("System Information");
             embed.setColor(0x00FFA200);
             embed.addField("Brand:", Build.BRAND, true);
-            embed.addField("Version:", Build.VERSION.RELEASE, true);
-            embed.addField("Codename:", Build.VERSION.CODENAME, true);
+            embed.addField("Product:", Build.PRODUCT, true);
+            embed.addField("Bootloader:", Build.BOOTLOADER, true);
+            embed.addField("Board:", Build.BOARD, true);
+            embed.addField("OS Version:", Build.VERSION.RELEASE, true);
+            embed.addField("OS Codename:", Build.VERSION.CODENAME, true);
+            embed.addField("Total Memory:", Math.ceil(totalMemory / 1e+9) + " GB", true);
+            embed.addField("Uptime:", uptime, true);
 
             return new CommandsAPI.CommandResult(null, Collections.singletonList(embed.build()), false);
         });
---
+++ WiderScrollbar/src/main/java/com/aliucord/plugins/WiderScrollbar.java
@@ -0,0 +1,103 @@
+package com.aliucord.plugins;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.aliucord.Utils;
+import com.aliucord.api.SettingsAPI;
+import com.aliucord.entities.Plugin;
+import com.aliucord.fragments.SettingsPage;
+import com.aliucord.patcher.PinePatchFn;
+import com.aliucord.views.TextInput;
+import com.discord.databinding.WidgetChatListBinding;
+import com.discord.widgets.chat.list.WidgetChatList;
+import com.discord.widgets.chat.list.model.WidgetChatListModel;
+
+import java.lang.reflect.InvocationTargetException;
+
+@SuppressWarnings("unused")
+public class WiderScrollbar extends Plugin {
+    public WiderScrollbar () {
+        settingsTab = new SettingsTab(PluginSettings.class).withArgs(settings);
+    }
+
+    public static class PluginSettings extends SettingsPage {
+        private final SettingsAPI settings;
+        public PluginSettings(SettingsAPI settings) {
+            this.settings = settings;
+        }
+
+        @Override
+        @SuppressWarnings("ResultOfMethodCallIgnored")
+        public void onViewBound(View view) {
+            super.onViewBound(view);
+            setActionBarTitle("Wider Scrollbar");
+
+            var context = view.getContext();
+            var input = new TextInput(context);
+
+            input.setHint("Scrollbar width in dp");
+
+            var editText = input.getEditText();
+            assert editText != null;
+
+            editText.setMaxLines(1);
+            editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+            editText.setText(String.valueOf(settings.getInt("scrollbarWidth", 50)));
+            editText.addTextChangedListener(new TextWatcher() {
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+                public void onTextChanged(CharSequence s, int start, int before, int count) { }
+                public void afterTextChanged(Editable s) {
+                    try {
+                        int width = Integer.parseInt(s.toString());
+                        if (width != 0) settings.setInt("scrollbarWidth", width);
+                    } catch (Throwable ignored) {
+                        settings.setInt("scrollbarWidth", 50);
+                    }
+                }
+            });
+
+            addView(input);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Manifest getManifest() {
+        Manifest manifest = new Manifest();
+        manifest.authors = new Manifest.Author[]{ new Manifest.Author("Möth", 289556910426816513L) };
+        manifest.description = "Allows changing the scrollbar width to make it easier to drag.";
+        manifest.version = "1.0.0";
+        manifest.updateUrl = "https://raw.githubusercontent.com/litleck/aliucord-plugins/builds/updater.json";
+        return manifest;
+    }
+
+    @Override
+    public void start(Context context) throws NoSuchMethodException {
+        final var getBindingMethod = WidgetChatList.class.getDeclaredMethod("getBinding");
+        getBindingMethod.setAccessible(true);
+
+        patcher.patch(WidgetChatList.class.getDeclaredMethod("configureUI", WidgetChatListModel.class), new PinePatchFn(callFrame -> {
+            WidgetChatListBinding widgetChatListBinding = null;
+
+            try {
+                widgetChatListBinding = (WidgetChatListBinding) getBindingMethod.invoke(callFrame.thisObject);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+
+            if (widgetChatListBinding == null) return;
+            widgetChatListBinding.a.setScrollBarSize(settings.getInt("scrollbarWidth", Utils.dpToPx(50)));
+        }));
+    }
+
+    @Override
+    public void stop(Context context) {
+        patcher.unpatchAll();
+    }
+}

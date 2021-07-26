c23702366eff58b5883fa3ca2a3ef01432217d54
litleck <nwowens32@gmail.com>
2021-07-25 22:38:37 -0400

    Added TypingUser stub

---
+++ TypingIndicators/src/main/java/com/discord/api/user/TypingUser.java
@@ -0,0 +1,9 @@
+package com.discord.api.user;
+
+public class TypingUser {
+    public final long a() {
+        return 0;
+    }
+    public final long b() { return 0; }
+    public final long d() { return 0; }
+}

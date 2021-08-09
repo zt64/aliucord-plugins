526be5eeb28c86f7258a4e326bde9fcbf2c7e45d
litleck <nwowens32@gmail.com>
2021-08-09 18:03:38 -0400

    Added AvatarMention plugin

---
+++ AvatarMention/src/main/java/com/discord/widgets/chat/list/adapter/WidgetChatListAdapterItemMessage$onConfigure$5.java
@@ -0,0 +1,8 @@
+package com.discord.widgets.chat.list.adapter;
+
+import com.discord.models.message.Message;
+
+public class WidgetChatListAdapterItemMessage$onConfigure$5 {
+    public Message $message;
+    public WidgetChatListAdapterItemMessage this$0;
+}
---
+++ AvatarMention/src/main/java/com/discord/widgets/chat/list/adapter/WidgetChatListAdapterItemMessage.java
@@ -0,0 +1,7 @@
+package com.discord.widgets.chat.list.adapter;
+
+public class WidgetChatListAdapterItemMessage {
+    public static WidgetChatListAdapter access$getAdapter$p(WidgetChatListAdapterItemMessage widgetChatListAdapterItemMessage) {
+        return access$getAdapter$p(widgetChatListAdapterItemMessage);
+    }
+}

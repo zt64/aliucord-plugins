f5be68c3f6a345c1c8e8395a2a10308621923a53
litleck <nwowens32@gmail.com>
2021-08-10 23:27:01 -0400

    Code cleanup

--- WiderScrollbar/src/main/java/com/aliucord/plugins/WiderScrollbar.java
+++ WiderScrollbar/src/main/java/com/aliucord/plugins/WiderScrollbar.java
@@ -5,6 +5,7 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 
@@ -18,32 +19,29 @@ import com.discord.databinding.WidgetChatListBinding;
 import com.discord.widgets.chat.list.WidgetChatList;
 import com.discord.widgets.chat.list.model.WidgetChatListModel;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-@SuppressWarnings("unused")
 public class WiderScrollbar extends Plugin {
-    public WiderScrollbar () {
-        settingsTab = new SettingsTab(PluginSettings.class).withArgs(settings);
-    }
+    public WiderScrollbar () { settingsTab = new SettingsTab(PluginSettings.class).withArgs(settings); }
 
     public static class PluginSettings extends SettingsPage {
         private final SettingsAPI settings;
+
         public PluginSettings(SettingsAPI settings) {
             this.settings = settings;
         }
 
-        @Override
         @SuppressWarnings("ResultOfMethodCallIgnored")
         public void onViewBound(View view) {
             super.onViewBound(view);
+
             setActionBarTitle("Wider Scrollbar");
+            setPadding(0);
 
-            var context = view.getContext();
-            var input = new TextInput(context);
-
+            TextInput input = new TextInput(requireContext());
             input.setHint("Scrollbar width in dp");
 
-            var editText = input.getEditText();
+            EditText editText = input.getEditText();
             assert editText != null;
 
             editText.setMaxLines(1);
@@ -51,7 +49,9 @@ public class WiderScrollbar extends Plugin {
             editText.setText(String.valueOf(settings.getInt("scrollbarWidth", 50)));
             editText.addTextChangedListener(new TextWatcher() {
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
                 public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
                 public void afterTextChanged(Editable s) {
                     try {
                         int width = Integer.parseInt(s.toString());
@@ -70,34 +70,26 @@ public class WiderScrollbar extends Plugin {
     @Override
     public Manifest getManifest() {
         Manifest manifest = new Manifest();
-        manifest.authors = new Manifest.Author[]{ new Manifest.Author("Möth", 289556910426816513L) };
+        manifest.authors = new Manifest.Author[]{ new Manifest.Author("zt", 289556910426816513L) };
         manifest.description = "Allows changing the scrollbar width to make it easier to drag.";
-        manifest.version = "1.0.1";
-        manifest.updateUrl = "https://raw.githubusercontent.com/litleck/aliucord-plugins/builds/updater.json";
+        manifest.version = "1.0.2";
+        manifest.updateUrl = "https://raw.githubusercontent.com/zt64/aliucord-plugins/builds/updater.json";
         return manifest;
     }
 
     @Override
     public void start(Context context) throws NoSuchMethodException {
-        final var getBindingMethod = WidgetChatList.class.getDeclaredMethod("getBinding");
+        final Method getBindingMethod = WidgetChatList.class.getDeclaredMethod("getBinding");
         getBindingMethod.setAccessible(true);
 
         patcher.patch(WidgetChatList.class.getDeclaredMethod("configureUI", WidgetChatListModel.class), new PinePatchFn(callFrame -> {
-            WidgetChatListBinding widgetChatListBinding = null;
-
             try {
-                widgetChatListBinding = (WidgetChatListBinding) getBindingMethod.invoke(callFrame.thisObject);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
-
-            if (widgetChatListBinding == null) return;
-            widgetChatListBinding.getRoot().setScrollBarSize(settings.getInt("scrollbarWidth", Utils.dpToPx(50)));
+                WidgetChatListBinding widgetChatListBinding = (WidgetChatListBinding) getBindingMethod.invoke(callFrame.thisObject);
+                if (widgetChatListBinding != null) widgetChatListBinding.getRoot().setScrollBarSize(settings.getInt("scrollbarWidth", Utils.dpToPx(50)));
+            } catch (Throwable ignored) { }
         }));
     }
 
     @Override
-    public void stop(Context context) {
-        patcher.unpatchAll();
-    }
+    public void stop(Context context) { patcher.unpatchAll(); }
 }

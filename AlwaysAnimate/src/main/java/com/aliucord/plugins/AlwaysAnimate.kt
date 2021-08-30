f86e2294fc07709baaa7ec36508dd709b50b267e
litleck <nwowens32@gmail.com>
2021-08-29 22:48:37 -0400

    Extended AlwaysAnimate functionality to all server icons

--- AlwaysAnimate/src/main/java/com/aliucord/plugins/AlwaysAnimate.kt
+++ AlwaysAnimate/src/main/java/com/aliucord/plugins/AlwaysAnimate.kt
@@ -1,24 +1,14 @@
 package com.aliucord.plugins
 
 import android.content.Context
-import android.widget.ImageView
 import com.aliucord.entities.Plugin
 import com.aliucord.entities.Plugin.Manifest.Author
 import com.aliucord.patcher.PinePrePatchFn
 import com.aliucord.plugins.alwaysanimate.PluginSettings
-import com.discord.models.guild.Guild
-import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
-import com.discord.widgets.guilds.list.GuildListViewHolder
-import java.lang.reflect.Field
+import com.discord.utilities.icon.IconUtils
+
 
 class AlwaysAnimate : Plugin() {
-    private var itemAvatarField: Field? = null
-    private val WidgetChatListAdapterItemMessage.itemAvatar: ImageView
-        get() = (itemAvatarField ?: javaClass.getDeclaredField("binding").apply {
-            isAccessible = true
-            itemAvatarField = this
-        })[this] as ImageView
-
     init {
         settingsTab = SettingsTab(PluginSettings::class.java, SettingsTab.Type.BOTTOM_SHEET).withArgs(settings)
     }
@@ -27,33 +17,21 @@ class AlwaysAnimate : Plugin() {
         return Manifest().apply {
             authors = arrayOf(Author("zt", 289556910426816513L))
             description = "Allows making server icons and member avatars always animate."
-            version = "1.0.2"
+            version = "1.1.0"
             updateUrl = "https://raw.githubusercontent.com/zt64/aliucord-plugins/builds/updater.json"
         }
     }
 
     override fun start(context: Context) {
-        if (settings.getBool("guildIcon", true)) {
-            patcher.patch(GuildListViewHolder.GuildViewHolder::class.java.getDeclaredMethod("configureGuildIconImage", Guild::class.java, Boolean::class.javaPrimitiveType), PinePrePatchFn {
-                it.args[1] = true
+        if (settings.getBool("guildIcons", true)) {
+            patcher.patch(IconUtils::class.java.getDeclaredMethod("getForGuild", Long::class.javaObjectType, String::class.javaObjectType, String::class.javaObjectType, Boolean::class.java, Int::class.javaObjectType), PinePrePatchFn {
+                it.args[3] = true
             })
         }
 
-//        if (settings.getBool("messageAvatar", true)) {
-//            val iconUtils = IconUtils.INSTANCE
-//
-//            patcher.patch(WidgetChatListAdapterItemMessage::class.java.getDeclaredMethod("onConfigure", Int::class.javaPrimitiveType, ChatListEntry::class.java), PinePatchFn { callFrame ->
-//                val messageEntry = callFrame.args[1] as MessageEntry
-//
-//                if (messageEntry.message.author == null) return@PinePatchFn
-//
-//                val coreUser = CoreUser(messageEntry.message.author)
-//                coreUser.avatar?.let {
-//                    if (iconUtils.isImageHashAnimated(it)) {
-//                        val itemAvatar = (callFrame.thisObject as WidgetChatListAdapterItemMessage).itemAvatar
-//                        val avatarUrl = "cdn.discordapp.com/avatars/${coreUser.id}/${coreUser.avatar}.gif"
-//                    }
-//                }
+//        if (settings.getBool("avatars", true)) {
+//            patcher.patch(IconUtils::class.java.getDeclaredMethod("getForUser", Long::class.javaObjectType, String::class.javaObjectType, Int::class.javaObjectType, Boolean::class.javaPrimitiveType, Int::class.javaObjectType), PinePrePatchFn {
+//                it.args[3] = true
 //            })
 //        }
     }

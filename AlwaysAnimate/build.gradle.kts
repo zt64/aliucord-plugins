6952d31008b2e452db2e8090d52db25ba5ab7c28
HalalStuff <darthsion626@gmail.com>
2021-09-06 12:36:00 -0500

    Bump AlwaysAnimate version and update changelog

--- AlwaysAnimate/build.gradle.kts
+++ AlwaysAnimate/build.gradle.kts
@@ -1,10 +1,11 @@
-version = "1.3.1"
+version = "1.4.0"
 description = "Allows making server icons and user avatars always animate"
 
 aliucord {
     changelog.set(
     """
-        * Fixed some bugs
+        * Added battery saver setting to automatically disable animations when battery saver is on
+        * Fixed compatibility issue with SquareAvatars
     """.trimIndent()
     )
 }

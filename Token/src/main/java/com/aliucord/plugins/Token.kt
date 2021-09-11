07951ff88047fa11df259000b0b612f518e8cde2
halalstuff <23338617+terabyte25@users.noreply.github.com>
2021-09-11 18:07:42 -0500

    Add send option to /token

--- Token/src/main/java/com/aliucord/plugins/Token.kt
+++ Token/src/main/java/com/aliucord/plugins/Token.kt
@@ -5,15 +5,20 @@ import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.api.CommandsAPI.CommandResult
 import com.aliucord.entities.Plugin
 import com.aliucord.utils.ReflectUtils
+import com.discord.api.commands.ApplicationCommandType
+import com.discord.models.commands.ApplicationCommandOption
 import com.discord.stores.StoreStream
+import java.util.*
 
 @AliucordPlugin
 class Token : Plugin() {
     override fun start(context: Context) {
-        commands.registerCommand("token", "Tells you your token", emptyList()) {
+        commands.registerCommand("token", "Tells you your token", Collections.singletonList(
+            ApplicationCommandOption(ApplicationCommandType.BOOLEAN, "send", "Whether the result should be visible for everyone", null, false, false, null, null)
+        )) {
             try {
                 val token = ReflectUtils.getField(StoreStream.getAuthentication(), "authToken") as String?
-                return@registerCommand CommandResult("```\n$token```", null, false)
+                return@registerCommand CommandResult("```\n$token```", null, it.getBoolOrDefault("send", false))
             } catch (e: NoSuchFieldException) {
                 e.printStackTrace()
                 return@registerCommand CommandResult("Uh oh, failed to get token", null, false)

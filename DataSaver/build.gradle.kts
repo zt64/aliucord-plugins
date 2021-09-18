9d1a93a6b8ba56329c1d4082993545ca8090e6f3
litleck <nwowens32@gmail.com>
2021-09-18 13:03:52 -0400

    Updated DataSaver description to be more clear

--- DataSaver/build.gradle.kts
+++ DataSaver/build.gradle.kts
@@ -1,2 +1,2 @@
-version = "1.1.1"
-description = "Disables media previews when on mobile data to limit data usage."
+version = "1.2.0"
+description = "Disables inline images and videos in chat in the Text & Images settings when on mobile data to limit data usage"

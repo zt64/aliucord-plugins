1b45b47d98125bc15bc1b802e6e968c0aaec3bd6
litleck <nwowens32@gmail.com>
2021-09-21 16:20:47 -0400

    Updated gradle and kotlin gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         classpath("com.github.Aliucord:gradle:main-SNAPSHOT")
     }
 }

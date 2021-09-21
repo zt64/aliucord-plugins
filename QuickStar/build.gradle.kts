5194bfdc4542e4896558cc0fdee9a2b8ba483fc9
litleck <nwowens32@gmail.com>
2021-09-21 17:55:28 -0400

    Cleaned up QuickStar code

--- QuickStar/build.gradle.kts
+++ QuickStar/build.gradle.kts
@@ -1,2 +1,2 @@
-version = "1.2.3"
+version = "1.3.0"
 description = "Adds a star button to the message context menu that reacts to the message with the star emoji."
--- QuickStar/src/main/java/com/aliucord/plugins/QuickStar.kt
+++ QuickStar/src/main/java/com/aliucord/plugins/QuickStar.kt
@@ -8,6 +8,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.widget.NestedScrollView
+import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.PinePatchFn
@@ -17,43 +18,41 @@ import com.discord.stores.StoreStream
 import com.discord.utilities.color.ColorCompat
 import com.discord.widgets.chat.list.actions.WidgetChatListActions
 import com.lytefast.flexinput.R
-import top.canyie.pine.Pine.CallFrame
-import java.lang.reflect.InvocationTargetException
 
 @AliucordPlugin
 class QuickStar : Plugin() {
+    private val getBindingMethod = WidgetChatListActions::class.java.getDeclaredMethod("getBinding").apply {
+        isAccessible = true
+    }
+
+    private fun WidgetChatListActions.getBinding(): WidgetChatListActionsBinding =
+            getBindingMethod.invoke(this) as WidgetChatListActionsBinding
+
+    private fun WidgetChatListActions.addReaction(emoji: Emoji) =
+            WidgetChatListActions.`access$addReaction`(this, emoji)
+
     @SuppressLint("SetTextI18n")
     override fun start(context: Context) {
         val icon = ContextCompat.getDrawable(context, R.d.ic_star_24dp)
         val quickStarId = View.generateViewId()
 
         with(WidgetChatListActions::class.java, {
-            val getBinding = getDeclaredMethod("getBinding").apply { isAccessible = true }
-            val addReaction = getDeclaredMethod("addReaction", Emoji::class.java).apply { isAccessible = true }
-
-            patcher.patch(getDeclaredMethod("configureUI", WidgetChatListActions.Model::class.java), PinePatchFn { callFrame: CallFrame ->
-                try {
-                    val binding = getBinding.invoke(callFrame.thisObject) as WidgetChatListActionsBinding
-                    val quickStar = binding.a.findViewById<TextView>(quickStarId).apply {
-                        visibility = if ((callFrame.args[0] as WidgetChatListActions.Model).manageMessageContext.canAddReactions) View.VISIBLE else View.GONE
+            patcher.patch(getDeclaredMethod("configureUI", WidgetChatListActions.Model::class.java), PinePatchFn {
+                with (it.thisObject as WidgetChatListActions) {
+                    val root = getBinding().root.findViewById<LinearLayout>(Utils.getResId("dialog_chat_actions_container", "id"))
+                    val quickStar = root.findViewById<TextView>(quickStarId).apply {
+                        visibility = if ((it.args[0] as WidgetChatListActions.Model).manageMessageContext.canAddReactions) View.VISIBLE else View.GONE
                     }
 
-                    if (!quickStar.hasOnClickListeners()) quickStar.setOnClickListener {
-                        try {
-                            addReaction.invoke(callFrame.thisObject, StoreStream.getEmojis().unicodeEmojisNamesMap["star"])
-                            (callFrame.thisObject as WidgetChatListActions).dismiss()
-                        } catch (e: IllegalAccessException) {
-                            e.printStackTrace()
-                        } catch (e: InvocationTargetException) {
-                            e.printStackTrace()
-                        }
+                    quickStar.setOnClickListener {
+                        addReaction(StoreStream.getEmojis().unicodeEmojisNamesMap["star"]!!)
+                        dismiss()
                     }
-                } catch (ignored: Throwable) {
                 }
             })
 
-            patcher.patch(getDeclaredMethod("onViewCreated", View::class.java, Bundle::class.java), PinePatchFn { callFrame: CallFrame ->
-                val linearLayout = (callFrame.args[0] as NestedScrollView).getChildAt(0) as LinearLayout
+            patcher.patch(getDeclaredMethod("onViewCreated", View::class.java, Bundle::class.java), PinePatchFn {
+                val linearLayout = (it.args[0] as NestedScrollView).getChildAt(0) as LinearLayout
                 val ctx = linearLayout.context
 
                 icon?.setTint(ColorCompat.getThemedColor(ctx, R.b.colorInteractiveNormal))

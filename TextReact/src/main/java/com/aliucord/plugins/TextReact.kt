e7341a0022993249590e8d4da74e7cdf04a837a4
ArjixWasTaken <arjixg53@gmail.com>
2021-09-21 21:20:30 +0300

    uhh x9999

--- TextReact/src/main/java/com/aliucord/plugins/TextReact.kt
+++ TextReact/src/main/java/com/aliucord/plugins/TextReact.kt
@@ -87,7 +87,6 @@ class TextReact : Plugin() {
                 val quickStar = TextView(ctx, null, 0, R.h.UiKit_Settings_Item_Icon).apply {
                     text = "Text react"
                     id = quickStarId
-                    visibility = View.VISIBLE
                     setCompoundDrawablesRelativeWithIntrinsicBounds(icon, null, null, null)
                 }
 

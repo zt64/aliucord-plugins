cceb1dece7f7884e95c3ef2279427d19634d1b17
litleck <nwowens32@gmail.com>
2021-10-03 15:06:44 -0400

    Release CustomNoticeDuration

---
+++ CustomNoticeDuration/src/main/java/tk/zt64/plugins/CustomNoticeTime.kt
@@ -0,0 +1,28 @@
+package tk.zt64.plugins
+
+import android.animation.ValueAnimator
+import android.content.Context
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.entities.Plugin
+import com.aliucord.patcher.PinePatchFn
+import com.discord.widgets.notice.NoticePopup
+import tk.zt64.plugins.customnoticetime.PluginSettings
+
+@AliucordPlugin
+class CustomNoticeDuration: Plugin() {
+    init {
+        settingsTab = SettingsTab(PluginSettings::class.java, SettingsTab.Type.BOTTOM_SHEET).withArgs(settings)
+    }
+
+    override fun start(context: Context) {
+        patcher.patch(NoticePopup::class.java.getDeclaredMethod("getAutoDismissAnimator", Integer::class.javaObjectType, Function0::class.java), PinePatchFn {
+            val result = it.result as ValueAnimator
+            if (settings.getBool("autoDismissNotice", true))
+                result.duration = settings.getLong("noticeDuration", 5000)
+            else
+                result.cancel()
+        })
+    }
+
+    override fun stop(context: Context) = patcher.unpatchAll()
+}

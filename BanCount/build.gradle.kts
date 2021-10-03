644c0115c68ed22a905975b798275dee423b3f4c
litleck <nwowens32@gmail.com>
2021-10-03 13:50:07 -0400

    Release BanCount plugin

---
+++ BanCount/build.gradle.kts
@@ -0,0 +1,2 @@
+version = "1.0.0"
+description = "Shows the total number of banned users on the bans page in the server settings"
---
+++ BanCount/src/main/java/tk/zt64/plugins/BanCount.kt
@@ -0,0 +1,22 @@
+package tk.zt64.plugins
+
+import android.content.Context
+import com.aliucord.Utils
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.entities.Plugin
+import com.aliucord.patcher.PinePatchFn
+import com.discord.widgets.servers.WidgetServerSettingsBans
+
+@AliucordPlugin
+class BanCount: Plugin() {
+    override fun start(context: Context) {
+        patcher.patch(WidgetServerSettingsBans::class.java.getDeclaredMethod("configureUI", WidgetServerSettingsBans.Model::class.java), PinePatchFn {
+            val model = it.args[0] as WidgetServerSettingsBans.Model
+            with(it.thisObject as WidgetServerSettingsBans) {
+                actionBarTitleLayout.setSubtitle("${model.guildName} (${Utils.pluralise(model.totalBannedUsers, "ban")})")
+            }
+        })
+    }
+
+    override fun stop(context: Context) = patcher.unpatchAll()
+}

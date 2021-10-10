3cd14683fcc804959d189d1089d0fa3c18addeea
litleck <nwowens32@gmail.com>
2021-10-09 21:12:55 -0400

    Temporarily disabled CursorInput

--- CursorInput/build.gradle.kts
+++ CursorInput/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.2.0"
+version = "1.2.1"
 description = "Makes it so that emojis, mentions and others are added at the cursor position instead of the end of the message"
 
 aliucord {
     changelog.set(
         """
-                
+            
+            # 1.2.1
+            * Disabled CursorInput as it has unexpected functionality on V96, and it's most likely an issue with the Pine hooking framework
+            
             # 1.2.0
             * Support V96
 
--- CursorInput/src/main/java/tk/zt64/plugins/CursorInput.kt
+++ CursorInput/src/main/java/tk/zt64/plugins/CursorInput.kt
@@ -1,12 +1,9 @@
 package tk.zt64.plugins
 
 import android.content.Context
+import com.aliucord.Logger
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
-import com.aliucord.patcher.Hook
-import com.discord.widgets.chat.input.AppFlexInputViewModel
-import com.lytefast.flexinput.R
-import com.lytefast.flexinput.fragment.`FlexInputFragment$c`
 import com.lytefast.flexinput.widget.FlexEditText
 
 @AliucordPlugin
@@ -14,27 +11,29 @@ class CursorInput: Plugin() {
     private var flexEditText: FlexEditText? = null
 
     override fun start(context: Context) {
-        patcher.patch(`FlexInputFragment$c`::class.java.getDeclaredMethod("invoke", Object::class.java), Hook {
-            flexEditText = (it.result as c.b.a.e.a).root.findViewById(R.e.text_input)
-        })
+        return Logger(this.getName()).warn("CursorInput is temporarily disabled due to issues with patching")
 
-        patcher.patch(AppFlexInputViewModel::class.java.getDeclaredMethod("onInputTextAppended", String::class.java), Hook {
-            with(it.thisObject as AppFlexInputViewModel) {
-                val baseString = requireViewState().a
-                var str = it.args[0] as String
-
-                if (flexEditText != null) {
-                    val selectionEnd = flexEditText!!.selectionEnd
-
-                    if (selectionEnd != baseString.length) str = str.trim()
-
-                    onInputTextChanged(StringBuilder(baseString).insert(selectionEnd, str).toString(), null)
-                    flexEditText!!.setSelection(selectionEnd + str.length)
-                } else {
-                    onInputTextChanged(baseString + str, null)
-                }
-            }
-        })
+//        patcher.patch(`FlexInputFragment$c`::class.java.getDeclaredMethod("invoke", Object::class.java), Hook {
+//            flexEditText = (it.result as c.b.a.e.a).root.findViewById(R.e.text_input)
+//        })
+//
+//        patcher.patch(AppFlexInputViewModel::class.java.getDeclaredMethod("onInputTextAppended", String::class.java), Hook {
+//            with(it.thisObject as AppFlexInputViewModel) {
+//                val baseString = requireViewState().a
+//                var str = it.args[0] as String
+//
+//                if (flexEditText != null) {
+//                    val selectionEnd = flexEditText!!.selectionEnd
+//
+//                    if (selectionEnd != baseString.length) str = str.trim()
+//
+//                    onInputTextChanged(StringBuilder(baseString).insert(selectionEnd, str).toString(), null)
+//                    flexEditText!!.setSelection(selectionEnd + str.length)
+//                } else {
+//                    onInputTextChanged(baseString + str, null)
+//                }
+//            }
+//        })
     }
 
     override fun stop(context: Context) {

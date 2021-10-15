3c70b22a364ee6be3ca2e10e16531da61e15976f
litleck <nwowens32@gmail.com>
2021-10-15 16:34:24 -0400

    Updated CharCounter

--- CharCounter/build.gradle.kts
+++ CharCounter/build.gradle.kts
@@ -1,9 +1,12 @@
-version = "1.1.0"
+version = "1.2.0"
 description = "Adds a character counter to the message box"
 
 aliucord {
     changelog.set(
         """
+            
+            # 1.2.0
+            * Counter is now properly placed in the bar
                 
             # 1.1.0
             * Support V96
--- CharCounter/src/main/java/com/aliucord/plugins/CharCounter.kt
+++ CharCounter/src/main/java/com/aliucord/plugins/CharCounter.kt
@@ -1,13 +1,13 @@
 package com.aliucord.plugins
 
 import android.content.Context
-import android.graphics.Color
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.content.res.ResourcesCompat
-import com.aliucord.Constants
 import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
@@ -16,38 +16,44 @@ import com.aliucord.utils.DimenUtils
 import com.discord.api.premium.PremiumTier
 import com.discord.databinding.WidgetChatOverlayBinding
 import com.discord.stores.StoreStream
+import com.discord.utilities.color.ColorCompat
 import com.discord.widgets.chat.input.AppFlexInputViewModel
 import com.discord.widgets.chat.overlay.`WidgetChatOverlay$binding$2`
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.lytefast.flexinput.R
 
 @AliucordPlugin
 class CharCounter : Plugin() {
     override fun start(context: Context) {
-        val counter = TextView(context).apply {
-            typeface = ResourcesCompat.getFont(context, Constants.Fonts.whitney_medium)
-            textSize = DimenUtils.dpToPx(4).toFloat()
-            visibility = View.GONE
-            layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
-                .apply { rightToRight = ConstraintLayout.LayoutParams.PARENT_ID }
-            setTextColor(Color.WHITE)
-        }
+        var counter: TextView? = null
 
         patcher.patch(`WidgetChatOverlay$binding$2`::class.java.getDeclaredMethod("invoke", View::class.java), Hook {
             val root = (it.result as WidgetChatOverlayBinding).root as ConstraintLayout
-            if (counter.parent == null) root.addView(counter)
-            val floatingActionButton = root.findViewById<FloatingActionButton>(Utils.getResId("chat_overlay_old_messages_fab", "id"))
-            ConstraintSet().apply {
-                clone(root)
-                connect(floatingActionButton.id, ConstraintSet.BOTTOM, counter.id, ConstraintSet.TOP)
-                applyTo(root)
+
+            counter = TextView(root.context, null, 0, R.h.UiKit_TextView).apply {
+                id = View.generateViewId()
+                visibility = View.GONE
+                gravity = Gravity.CENTER_VERTICAL
+                layoutParams = ConstraintLayout.LayoutParams(WRAP_CONTENT, DimenUtils.dpToPx(24)).apply {
+                    rightToRight = ConstraintLayout.LayoutParams.PARENT_ID
+                    bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
+                }
+                setTextSize(TypedValue.COMPLEX_UNIT_PX, root.resources.getDimension(Utils.getResId("uikit_textsize_small", "dimen")))
+                setBackgroundColor(ColorCompat.getThemedColor(root.context, R.b.primary_630))
+                root.addView(this)
             }
+
+            (root.findViewById<RelativeLayout>(Utils.getResId("chat_overlay_typing", "id"))
+                .layoutParams as ConstraintLayout.LayoutParams).apply {
+                    rightToLeft = counter!!.id
+                    width = WRAP_CONTENT
+                }
         })
 
         patcher.patch(AppFlexInputViewModel::class.java.getDeclaredMethod("onInputTextChanged", String::class.java, Boolean::class.javaObjectType), Hook {
             val str = it.args[0] as String
             val maxChars = if (StoreStream.getUsers().me.premiumTier == PremiumTier.TIER_2) "4000" else "2000"
 
-            counter.apply {
+            counter?.apply {
                 visibility = if (str == "") View.GONE else View.VISIBLE
                 text = String.format("%s/%s", str.length, maxChars)
             }

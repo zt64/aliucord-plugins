a0298aa9e504da7b5c1c8430f8ab6b3e521ec6d2
halalstuff <23338617+terabyte25@users.noreply.github.com>
2021-11-12 17:00:51 -0600

    Bump version

--- RoleColorEverywhere/build.gradle.kts
+++ RoleColorEverywhere/build.gradle.kts
@@ -1,11 +1,11 @@
-version = "1.8.0"
+version = "1.9.0"
 description = "Displays the highest role color in more places like mentions and typing text"
 
 aliucord {
     changelog.set(
         """
              
-            # 1.8.0
+            # 1.9.0
             * Added color to reaction username list
 
         """.trimIndent()

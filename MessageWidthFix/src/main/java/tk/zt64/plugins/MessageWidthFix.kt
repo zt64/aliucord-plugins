b8cb32cd7fe2e01460da3bb4cd4ecf6c32b3779f
litleck <nwowens32@gmail.com>
2022-01-24 22:08:43 -0500

    Updated MessageWidthFix to V1.0.2 * Support Discord V112.4

--- MessageWidthFix/src/main/java/tk/zt64/plugins/MessageWidthFix.kt
+++ MessageWidthFix/src/main/java/tk/zt64/plugins/MessageWidthFix.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.FrameLayout
 import android.widget.RelativeLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
 import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
@@ -14,21 +15,27 @@ import com.discord.widgets.chat.list.adapter.WidgetChatListItem
 import com.discord.widgets.chat.list.entries.ChatListEntry
 import com.discord.widgets.chat.list.entries.StickerEntry
 import com.google.android.material.card.MaterialCardView
+import com.lytefast.flexinput.R
 
 @AliucordPlugin
 class MessageWidthFix : Plugin() {
     override fun start(context: Context) {
         val containerCardViewId = Utils.getResId("chat_list_item_embed_container_card", "id")
-
         patcher.after<WidgetChatListItem>("onConfigure", Int::class.java, ChatListEntry::class.java) {
             val chatListEntry = it.args[1] as ChatListEntry
 
+            itemView.resources.getDimension(R.d.chat_cell_horizontal_spacing_padding)
+
             when (this) {
-                is WidgetChatListAdapterItemEmbed -> {
-                    (itemView as ConstraintLayout).layoutParams.width = RelativeLayout.LayoutParams.MATCH_PARENT
-                    (itemView.findViewById<MaterialCardView>(containerCardViewId).layoutParams as ConstraintLayout.LayoutParams).apply {
+                is WidgetChatListAdapterItemEmbed -> (itemView as ConstraintLayout).run {
+                    (layoutParams as RecyclerView.LayoutParams).apply {
+                        width = RecyclerView.LayoutParams.MATCH_PARENT
+                    }
+
+                    (findViewById<MaterialCardView>(containerCardViewId).layoutParams as ConstraintLayout.LayoutParams).apply {
                         width = ConstraintLayout.LayoutParams.WRAP_CONTENT
-                        endToEnd = ConstraintLayout.LayoutParams.UNSET
+                        horizontalBias = 0.0f
+                        constrainedWidth = true
                     }
                 }
                 is WidgetChatListAdapterItemSticker -> (itemView as FrameLayout).run {

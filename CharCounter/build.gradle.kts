2af4c6650c45ca920f2b4ae48ef78de7d49069f2
Javkhlanbayar Khongorzul <jbk@duck.com>
2022-08-30 12:43:42 +0200

    Bump CharCounter version to v1.4.1

--- CharCounter/build.gradle.kts
+++ CharCounter/build.gradle.kts
@@ -1,22 +1,25 @@
-version = "1.4.0"
+version = "1.4.1"
 description = "Adds a character counter to the message box"
 
 aliucord.changelog.set("""
+    # 1.4.1
+    * Improved the background to fit in with chat
+
     # 1.4.0
     * Added threshold option
     * Added reverse option
     * Added color to show when too many characters are entered
-   
+
     # 1.2.2
     * Fixed issue with background
     * Added an option to always show the counter even when theres no input text
 
     # 1.2.1
     * Improved appearance
-    
+
     # 1.2.0
     * Counter is now properly placed in the bar
-        
+
     # 1.1.0
     * Support V96
 """.trimIndent())

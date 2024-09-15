3b631c4767c40d471c67e0623cdeaed4904bced7
Nick <31907977+zt64@users.noreply.github.com>
2024-09-15 11:07:15 -0400

    Update RoleColorEverywhere to allow easier modification

--- plugin/RoleColorEverywhere/build.gradle.kts
+++ plugin/RoleColorEverywhere/build.gradle.kts
@@ -1,8 +1,11 @@
-version = "1.11.3"
+version = "1.12.0"
 description = "Displays the highest role color in more places like mentions and typing text"
 
 aliucord.changelog.set(
     """
+    # 1.12.0
+    * Added function to allow for easier modification by other plugins
+
     # 1.11.3
     * Fixed typo that was causing errors to occur
         

1b56b20a6a54ce17f9ffd55efd796bcd715ad012
zt64 <31907977+zt64@users.noreply.github.com>
2025-05-09 20:38:19 -0400

    refactor: rename AttachmentCounter plugin

--- plugin/AttachmentCounter/build.gradle.kts
+++
@@ -1,2 +0,0 @@
-version = "1.0.0"
-description = "Shows the total number of attachments selected in the media picker"
--- plugin/AttachmentSheetTweaks/src/main/kotlin/dev/zt64/aliucord/plugins/AttachmentSheetTweaks.kt
+++ plugin/AttachmentSheetTweaks/src/main/kotlin/dev/zt64/aliucord/plugins/AttachmentSheetTweaks.kt
@@ -1,3 +1,4 @@
+package dev.zt64.aliucord.plugins
 
 import android.content.Context
 import android.os.Bundle
@@ -5,16 +6,19 @@ import android.view.*
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.after
 import com.aliucord.utils.DimenUtils.dp
+import com.aliucord.widgets.BottomSheet
+import com.discord.views.CheckedSetting
 import com.lytefast.flexinput.R
+import com.lytefast.flexinput.fragment.FlexInputFragment
 import b.b.a.a.a as AddContentDialogFragment
 
-@Suppress("SetTextI18n")
 @AliucordPlugin
-class AttachmentCounter : Plugin() {
+class AttachmentSheetTweaks : Plugin() {
     override fun start(context: Context) {
         lateinit var textView: TextView
 
@@ -43,7 +47,44 @@ class AttachmentCounter : Plugin() {
             textView.visibility = if (selectionAggregator.size > 0) View.VISIBLE else View.GONE
             textView.text = "${selectionAggregator.size} selected"
         }
+
+        val m = FlexInputFragment::class.java
+            .getDeclaredMethod("g", FlexInputFragment::class.java, Int::class.java)
+
+        // patcher.patch(
+        //     m,
+        //     InsteadHook {
+        //         val fragment = it.args[0] as FlexInputFragment
+        //
+        //         val transaction = fragment.getChildFragmentManager().beginTransaction()
+        //
+        //         val sheet = B()
+        //
+        //         sheet.show(transaction, "Add content")
+        //     }
+        // )
     }
 
     override fun stop(context: Context) = patcher.unpatchAll()
+}
+
+@Suppress("MISSING_DEPENDENCY_SUPERCLASS")
+class B : BottomSheet() {
+    override fun onViewCreated(view: View, bundle: Bundle?) {
+        super.onViewCreated(view, bundle)
+
+        val ctx = requireContext()
+
+        addView(
+            Utils
+                .createCheckedSetting(
+                    context = ctx,
+                    type = CheckedSetting.ViewType.SWITCH,
+                    text = "Reverse",
+                    subtext = "Whether the counter goes in reverse, counting down how many chars remain"
+                ).apply {
+                    isChecked = false
+                }
+        )
+    }
 }

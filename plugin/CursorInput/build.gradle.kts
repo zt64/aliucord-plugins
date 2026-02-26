14326340b326659b5f2d82fd4dc8e28fc173569e
zt64 <31907977+zt64@users.noreply.github.com>
2026-02-25 22:55:20 -0500

    feat(CursorInput): release v1.4.3

--- plugin/CursorInput/build.gradle.kts
+++ plugin/CursorInput/build.gradle.kts
@@ -1,9 +1,14 @@
-version = "1.4.2"
+version = "1.4.3"
 description =
     "Makes it so that emojis, mentions and others are added at the cursor position instead of the end of the message"
 
 aliucord.changelog.set(
     """
+    # 1.4.3
+    * Fixed a bug which resulted in inserted content would be duplicated (credit to cillynder)
+        
+    # 1.4.2    
+    
     # 1.4.1
     * Support Discord V112.4
     """.trimIndent()
